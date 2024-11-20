52d725c6ce58d8de3b7bc6ae9cc25134690b794e
Cyril Soldani <c.soldani@helmo.be>
2024-11-20 15:41:55 +0100

    Change joke API

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.get('https://punapi.rest/api/pun')
+    r = requests.get('https://official-joke-api.appspot.com/random_joke')
     if r.status_code == 200:
         d = r.json()
-        print(d['pun'])
+        print(d['setup'])
+        print(d['punchline'])
     else:
-        print("Error fetching pun from PunAPI!", file=sys.stderr)
+        print("Error fetching joke!", file=sys.stderr)
         sys.exit(1)
